777127291e306d38aaace2c1c0d6d9afc0ec9f3e
John Gorman <johngorm@usc.edu>
2017-06-17 11:13:16 -0500

    Render serach history panel

--- app/components/children/History.js
+++ app/components/children/History.js
@@ -3,17 +3,16 @@ const React = require('react');
 const History = React.createClass({
 	
 	render: function(){
- 	<div className="panel panel-default">
+  return(  
+   	<div className="panel panel-default">
         <div className="panel-heading">
           <h3 className="panel-title text-center">Search Hisotry</h3>
         </div>
         <div className="panel-body text-center">
-          <h1>Address:</h1>
-          <p>{this.props.address}</p>
         </div>
     </div>		
 
-	}
+	)}
 });
 
 
