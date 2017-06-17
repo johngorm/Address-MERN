90b26513417c5913c476840b2fdd75026d150833
dangnabit <dan.gabel@gmail.com>
2017-06-17 11:07:44 -0500

    server

---
+++ model/address.js
@@ -0,0 +1,24 @@
+// Include the Mongoose Dependencies
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+// Create a Schema for capturing clicks. We'll use clickID to update the same clickCounter
+var AddressSchema = new Schema({
+  address: {
+    type: String
+  },
+  query: {
+    type: String
+  },
+  date: {
+  	type: Date,
+  	default: Date.now()
+  }
+});
+
+// Create the Model
+var Address = mongoose.model("Click", AddressSchema);
+
+// Export it for use elsewhere
+module.exports = Address;
